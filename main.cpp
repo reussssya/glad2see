87a9d959ff01f98c35e4f2220042307f5b92980b
reussssya <amireusya@gmail.com>
2024-03-17 23:46:35 +0300

    Trying imgui...

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
+#include "data/database.h"
 #include "common.h"
 
 #include "windows/mainwindow.h"
 #include "windows/joinwindow.h"
 
-#include "data/database.h"
 
 #include "discordrpc.h"
 #include "trackplayer.hpp"
@@ -28,7 +28,7 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int main(int argc, char* argv[])
 {
-    
+    //CDatabase db;
     
     //QIcon trayIcon = QIcon(":/data/img/trayIcon.ico");
     
@@ -163,7 +163,6 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-// Helper functions
 bool CreateDeviceWGL(HWND hWnd, WGL_WindowData* data)
 {
     HDC hDc = ::GetDC(hWnd);
@@ -192,15 +191,8 @@ void CleanupDeviceWGL(HWND hWnd, WGL_WindowData* data)
     wglMakeCurrent(nullptr, nullptr);
     ::ReleaseDC(hWnd, data->hDC);
 }
-
-// Forward declare message handler from imgui_impl_win32.cpp
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-// Win32 message handler
-// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
